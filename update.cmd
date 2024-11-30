9683805b2ebbf05457b69b442198bf216e804e32
Ekoeden <nikota.85@mail.ru>
2024-11-30 17:06:15 +0800

    update fix

--- update.cmd
+++ update.cmd
@@ -1,6 +1,10 @@
 @echo off
 cd /d "%~dp0bin"
-python update.py
+
+timeout /t 2 >nul
+move /y "update.exe" "new_update.exe"
+"new_update.exe"
+del "new_update.exe"
 
 :: Проверяем статус службы WinSW
 for /f "tokens=*" %%a in ('WinSW.exe status') do set status=%%a
