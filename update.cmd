c43a4b75cda491cf244ef75b1a490af794099ed8
Ekoeden <117777165+Ekoeden@users.noreply.github.com>
2024-11-30 17:36:00 +0800

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,18 +1,18 @@
 @echo off
 cd /d "%~dp0bin"
 
-timeout /t 2 >nul
-move /y "update.exe" "new_update.exe" >nul
-
-:: Проверяем статус службы WinSW
 for /f "tokens=*" %%a in ('WinSW.exe status') do set status=%%a
 if "%status%"=="Started" (
     WinSW.exe stop
-    "new_update.exe"
-    WinSW.exe start
-) else (
-    "new_update.exe"
 )
 
+timeout /t 2 >nul
+move /y "update.exe" "new_update.exe" >nul
+"new_update.exe"
 del "new_update.exe"
+
+if "%status%"=="Started" (
+    WinSW.exe start
+)
+
 pause
