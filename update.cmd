af8f8fbf96502ccb5ce7a8b4a7fe138dcc846cdb
Ekoeden <117777165+Ekoeden@users.noreply.github.com>
2024-11-30 16:06:44 +0800

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,11 +1,15 @@
 @echo off
 cd /d "%~dp0bin"
-update.exe
+chcp 65001 >nul
 
 :: Проверяем статус службы WinSW
 for /f "tokens=*" %%a in ('WinSW.exe status') do set status=%%a
 if "%status%"=="Started" (
-    WinSW.exe restart
+    WinSW.exe stop
+    update.exe
+    WinSW.exe start
+) else (
+    update.exe
 )
 
 pause
